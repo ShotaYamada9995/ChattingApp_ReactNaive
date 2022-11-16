9b198ce58d8a0f3781f76a3a441b326266c7356f
yamada <starkdevmljp@gmail.com>
2022-11-15 23:09:03 -0800

    update: media post page ui

--- src/screens/CreateMedia/Post/index.tsx
+++ src/screens/CreateMedia/Post/index.tsx
@@ -5,25 +5,13 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
-  Switch,
-  TextInput,
-  KeyboardAvoidingView,
-  Platform,
   ActivityIndicator,
-  FlatList,
+  ScrollView,
 } from 'react-native';
-import {
-  Icon,
-  Button,
-  BottomSheet,
-  CheckBox,
-  Avatar,
-  LinearProgress,
-} from '@rneui/themed';
+import {Icon, Button, BottomSheet} from '@rneui/themed';
 import {useNavigation} from '@react-navigation/native';
 import {useDispatch, useSelector} from 'react-redux';
 import {MentionInput} from 'react-native-controlled-mentions';
-import {Video as VideoCompressor} from 'react-native-compressor';
 import CircularProgress from 'react-native-circular-progress-indicator';
 
 import MediaRepository from '../../../repositories/MediaRepository';
@@ -34,9 +22,8 @@ import {genFrameAt} from '../../../utils/videoProcessor';
 
 import {addThumbnail} from '../../../store/reducers/Video';
 
-import TagPeople from './TagPeople';
 import {useToast} from 'react-native-toast-notifications';
-import {WINDOW_WIDTH} from '../../../utils';
+import {WINDOW_HEIGHT, WINDOW_WIDTH} from '../../../utils';
 
 type Viewer = 'Everyone' | 'Friends' | 'Only me';
 
@@ -49,79 +36,28 @@ const PostMedia = () => {
     video: state.video,
   }));
 
-  const [config, setConfig] = useState({
-    allowComments: true,
-    allowDuet: true,
-    allowShare: true,
-  });
-  const [showViewers, setShowViewers] = useState(false);
-  const [showTagScreen, setShowTagScreen] = useState(false);
-  const [showHashtags, setShowHashtags] = useState(false);
-  const [showMentions, setShowMentions] = useState(false);
   const [isUploading, setIsUploading] = useState(false);
 
   const [uploadProgress, setUploadProgress] = useState(0);
-  const [viewer, setViewer] = useState<Viewer>('Everyone');
   const [caption, setCaption] = useState('');
-  const [tags, setTags] = useState([]);
-  const [mentions, setMentions] = useState<any[]>([
-    {_id: 1},
-    {_id: 2},
-    {_id: 3},
-    {_id: 4},
-    {_id: 5},
-    {_id: 6},
-    {_id: 7},
-    {_id: 8},
-    {_id: 9},
-  ]);
 
   const captionRef = useRef(null);
 
-  const toggleComments = () => {
-    setConfig(config => ({...config, allowComments: !config.allowComments}));
-  };
-
-  const toggleDuet = () => {
-    setConfig(config => ({...config, allowDuet: !config.allowDuet}));
-  };
-
-  const toggleShare = () => {
-    setConfig(config => ({...config, allowShare: !config.allowShare}));
-  };
-
-  const selectViewer = (viewer: Viewer) => {
-    setViewer(viewer);
-    setShowViewers(false);
-  };
-
   const handleOnChangeCaption = (caption: string) => {
-    if (caption[caption.length - 1] === '@') {
-      setShowMentions(true);
-    } else if (showMentions) {
-      setShowMentions(false);
-    }
-
-    if (caption[caption.length - 1] === '#') {
-      setShowHashtags(true);
-    } else if (showHashtags) {
-      setShowHashtags(false);
-    }
-
     setCaption(caption);
+  };
+
+  const addMention = () => {
+    setCaption(caption => caption + '@');
+  };
+
+  const addHashtag = () => {
+    setCaption(caption => caption + '#');
   };
 
   const postMedia = async () => {
     setIsUploading(true);
     try {
-      // const compressedVideo = await VideoCompressor.compress(
-      //   video.path,
-      //   {compressionMethod: 'auto'},
-      //   progress => {
-      //     console.log(`Compression Progress: ${Math.round(progress * 100)}%`);
-      //   },
-      // );
-
       const tags = caption
         .split(' ')
         .filter(word => word[0] === '#')
@@ -174,7 +110,7 @@ const PostMedia = () => {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <View style={[globalStyles.rowLayout, styles.header]}>
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <Icon name="arrow-back" />
@@ -183,236 +119,74 @@ const PostMedia = () => {
         <View />
       </View>
 
+      <View style={styles.thumbnailContainer}>
+        {video.thumbnail ? (
+          <Image
+            source={{uri: video.thumbnail}}
+            style={{width: '100%', height: '100%'}}
+            resizeMode="contain"
+          />
+        ) : (
+          <View
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <ActivityIndicator color="white" />
+          </View>
+        )}
+
+        <TouchableOpacity
+          style={styles.selectCoverBtn}
+          onPress={() =>
+            video.thumbnail ? navigation.navigate('SelectThumbnail') : null
+          }>
+          <Text style={styles.selectCoverText}>Select cover</Text>
+        </TouchableOpacity>
+      </View>
+
       <TouchableOpacity
         activeOpacity={1}
-        style={styles.captionCoverContaienr}
+        style={styles.captionContainer}
         onPress={() => captionRef.current?.focus()}>
-        <View style={styles.captionContainer}>
-          <MentionInput
-            inputRef={captionRef}
-            multiline
-            placeholder="Describe your post, add hashtags or mention creators that inspired you"
-            placeholderTextColor="grey"
-            value={caption}
-            onChange={handleOnChangeCaption}
-            style={styles.inputField}
-            partTypes={[
-              {
-                pattern: /(?<=#).*?(?=( |$))/g,
-                textStyle: {fontWeight: 'bold'},
-              },
-              {
-                pattern: /(?<=@).*?(?=( |$))/g,
-                textStyle: {fontWeight: 'bold'},
-              },
-            ]}
+        <MentionInput
+          inputRef={captionRef}
+          multiline
+          placeholder="Describe your post, add hashtags or mention creators that inspired you"
+          placeholderTextColor="grey"
+          value={caption}
+          onChange={handleOnChangeCaption}
+          style={styles.inputField}
+          partTypes={[
+            {
+              pattern: /(?<=#).*?(?=( |$))/g,
+              textStyle: {fontWeight: 'bold'},
+            },
+            {
+              pattern: /(?<=@).*?(?=( |$))/g,
+              textStyle: {fontWeight: 'bold'},
+            },
+          ]}
+        />
+
+        <View style={styles.captionFooter}>
+          <Button
+            title="Mention @"
+            type="outline"
+            buttonStyle={styles.captionFooterBtn}
+            titleStyle={styles.captionFooterBtnTxt}
+            onPress={addMention}
           />
-        </View>
-
-        <View style={styles.coverContainer}>
-          {video.thumbnail ? (
-            <Image
-              source={{uri: video.thumbnail}}
-              style={{width: '100%', height: '100%'}}
-              resizeMode="contain"
-            />
-          ) : null}
-
-          {!video.thumbnail ? (
-            <View
-              style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-              <ActivityIndicator color="white" />
-            </View>
-          ) : null}
-
-          <TouchableOpacity
-            style={styles.selectCoverBtn}
-            onPress={() =>
-              video.thumbnail ? navigation.navigate('SelectThumbnail') : null
-            }>
-            <Text style={styles.selectCoverText}>Select cover</Text>
-          </TouchableOpacity>
+          <Button
+            title="Hashtag #"
+            type="outline"
+            buttonStyle={styles.captionFooterBtn}
+            titleStyle={styles.captionFooterBtnTxt}
+            onPress={addHashtag}
+          />
         </View>
       </TouchableOpacity>
 
-      {showMentions ? (
-        <KeyboardAvoidingView style={{flex: 1}} behavior="position">
-          <FlatList
-            keyExtractor={(item, index) => item._id}
-            data={mentions}
-            renderItem={item => (
-              <TouchableOpacity
-                style={styles.mentionsContainer}
-                onPress={() => {
-                  setCaption(caption => caption + 'ValentineOrga');
-                  setShowMentions(false);
-                }}>
-                <Avatar
-                  source={require('../../../assets/images/profile_picture.webp')}
-                  rounded
-                  size="medium"
-                />
-
-                <View style={{marginLeft: 10}}>
-                  <Text style={styles.title}>Valentine Orga</Text>
-                  <Text style={styles.description}>@ValentineOrga</Text>
-                </View>
-              </TouchableOpacity>
-            )}
-          />
-        </KeyboardAvoidingView>
-      ) : showHashtags ? (
-        <FlatList
-          keyExtractor={(item, index) => item._id}
-          data={mentions}
-          renderItem={item => (
-            <TouchableOpacity
-              style={{padding: 10, width: '100%'}}
-              onPress={() => {
-                setCaption(caption => caption + 'WhatIDo');
-                setShowHashtags(false);
-              }}>
-              <Text style={styles.title}>#WhatIDo</Text>
-              <Text style={styles.description}>Lots of Tweets</Text>
-            </TouchableOpacity>
-          )}
-        />
-      ) : (
-        <KeyboardAvoidingView
-          style={{flex: 1}}
-          behavior={Platform.OS === 'ios' ? 'padding' : undefined}
-          keyboardVerticalOffset={Platform.OS === 'ios' ? 40 : 0}>
-          <TouchableOpacity
-            style={[styles.configLayout, globalStyles.rowLayout]}
-            onPress={() => setShowTagScreen(true)}>
-            <View style={styles.configEdgeLayout}>
-              {/* <Icon name="person-outline" type="ionicon" /> */}
-              <Text style={styles.label}>Tag people</Text>
-            </View>
-
-            <Icon name="chevron-right" color="black" />
-          </TouchableOpacity>
-
-          {/* <TagPeople
-            show={showTagScreen}
-            tags={tags}
-            setTags={setTags}
-            onCancel={() => setShowTagScreen(false)}
-          /> */}
-
-          {/* {user.isLoggedIn && (
-            <TouchableOpacity
-              style={[styles.configLayout, globalStyles.rowLayout]}>
-              <View style={styles.configEdgeLayout}> */}
-          {/* <Icon name="location-outline" type="ionicon" /> */}
-          {/* <Text style={styles.label}>Location</Text>
-              </View>
-
-              <Icon name="chevron-right" color="black" />
-            </TouchableOpacity>
-          )} */}
-
-          <TouchableOpacity
-            style={[styles.configLayout, globalStyles.rowLayout]}
-            onPress={() => setShowViewers(true)}>
-            <View style={styles.configEdgeLayout}>
-              {/* <Icon name="location-outline" type="ionicon" /> */}
-              <Text style={styles.label}>Who can watch this video?</Text>
-            </View>
-
-            <View style={styles.configEdgeLayout}>
-              <Text style={styles.selectedViewer}>{viewer}</Text>
-              <Icon name="chevron-right" color="black" />
-            </View>
-          </TouchableOpacity>
-
-          <BottomSheet
-            onBackdropPress={() => setShowViewers(false)}
-            isVisible={showViewers}>
-            <View style={styles.bottomSheetView}>
-              <View
-                style={[styles.bottomSheetViewHeader, globalStyles.rowLayout]}>
-                <View />
-                <Text style={styles.bottomSheetViewTitle}>Tags</Text>
-
-                <TouchableOpacity onPress={() => setShowViewers(false)}>
-                  <Icon name="close-outline" type="ionicon" size={30} />
-                </TouchableOpacity>
-              </View>
-
-              <TouchableOpacity
-                style={globalStyles.rowLayout}
-                onPress={() => selectViewer('Everyone')}>
-                <Text style={styles.label}>Everyone</Text>
-                <CheckBox checked={viewer === 'Everyone'} />
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={globalStyles.rowLayout}
-                onPress={() => selectViewer('Friends')}>
-                <Text style={styles.label}>Friends</Text>
-                <CheckBox checked={viewer === 'Friends'} />
-              </TouchableOpacity>
-
-              <TouchableOpacity
-                style={globalStyles.rowLayout}
-                onPress={() => selectViewer('Only me')}>
-                <Text style={styles.label}>Only me</Text>
-                <CheckBox checked={viewer === 'Only me'} />
-              </TouchableOpacity>
-            </View>
-          </BottomSheet>
-
-          <View style={[styles.configLayout, globalStyles.rowLayout]}>
-            <View style={styles.configEdgeLayout}>
-              {/* <Icon name="chatbubble-ellipses-outline" type="ionicon" /> */}
-              <Text style={styles.label}>Allow Comments</Text>
-            </View>
-
-            <Switch
-              trackColor={{false: '#767577', true: '#81b0ff'}}
-              thumbColor="#001433"
-              ios_backgroundColor="#3e3e3e"
-              onValueChange={toggleComments}
-              value={config.allowComments}
-            />
-          </View>
-
-          {/* <View style={[styles.configLayout, globalStyles.rowLayout]}>
-            <View style={styles.configEdgeLayout}>
-              <Icon name="location-pin" type="fonrawesome" />
-              <Text style={styles.label}>Allow Duet</Text>
-            </View>
-
-            <Switch
-              trackColor={{false: '#767577', true: '#81b0ff'}}
-              thumbColor="#001433"
-              ios_backgroundColor="#3e3e3e"
-              onValueChange={toggleDuet}
-              value={config.allowDuet}
-            />
-          </View> */}
-
-          <View style={[styles.configLayout, globalStyles.rowLayout]}>
-            <View style={styles.configEdgeLayout}>
-              {/* <Icon name="location-pin" type="fonrawesome" /> */}
-              <Text style={styles.label}>Allow Share</Text>
-            </View>
-
-            <Switch
-              trackColor={{false: '#767577', true: '#81b0ff'}}
-              thumbColor="#001433"
-              ios_backgroundColor="#3e3e3e"
-              onValueChange={toggleShare}
-              value={config.allowShare}
-            />
-          </View>
-        </KeyboardAvoidingView>
-      )}
-
       <Button
         title="Post"
-        containerStyle={styles.btn}
+        containerStyle={styles.postBtn}
         buttonStyle={{paddingVertical: 10, borderColor: '#001433'}}
         color="#001433"
         loading={isUploading}
@@ -447,7 +221,7 @@ const PostMedia = () => {
           />
         </View>
       </BottomSheet>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -458,26 +232,46 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
   },
   header: {
+    position: 'absolute',
+    top: 0,
+    width: '100%',
     paddingVertical: 5,
+    paddingHorizontal: 10,
   },
   headerText: {
     color: 'black',
     fontWeight: 'bold',
     fontSize: 20,
   },
-  captionCoverContaienr: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    height: '20%',
-  },
   captionContainer: {
-    flex: 2,
+    position: 'absolute',
+    bottom: 100,
+    width: '100%',
+    minHeight: WINDOW_HEIGHT * 0.2,
+    marginTop: 20,
+    borderWidth: 0.5,
+    borderColor: '#ccc',
+    backgroundColor: 'white',
+    alignSelf: 'center',
   },
+  captionFooter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 10,
+  },
+  captionFooterBtn: {
+    borderColor: 'black',
+    marginLeft: 10,
+  },
+  captionFooterBtnTxt: {color: 'black', fontSize: WINDOW_WIDTH * 0.04},
   inputField: {fontSize: 16, color: 'black'},
-  coverContainer: {
-    flex: 1,
+  thumbnailContainer: {
+    alignSelf: 'center',
+    width: '65%',
+    aspectRatio: 9 / 16,
+    marginTop: 50,
     backgroundColor: 'black',
-    aspectRatio: 3 / 4,
   },
   selectCoverBtn: {
     position: 'absolute',
@@ -485,11 +279,12 @@ const styles = StyleSheet.create({
     width: '100%',
     alignItems: 'center',
     padding: 10,
-    backgroundColor: 'rgba(0,0,0,0.5)',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   selectCoverText: {
     color: 'white',
     fontWeight: 'bold',
+    fontSize: WINDOW_WIDTH * 0.05,
   },
   text: {
     color: 'black',
@@ -522,7 +317,7 @@ const styles = StyleSheet.create({
     fontSize: 15,
     color: '#888',
   },
-  btn: {
+  postBtn: {
     width: '48%',
     marginTop: 50,
     alignSelf: 'center',
